3822d60b4183edadefee4ab4fd09d0d34f412270
ShravaniRoy <shravani.thirunagari@gmail.com>
2023-11-02 19:24:03 +0530

    adding a page jsx and adding scripts in package.json to render the page in layout with placeholders for header and footer

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return (
+        <>
+        <h1> School Reviews </h1>
+        <p>
+            Only best schools reviewed for you.
+        </p>
+        </>
+    )
+}
